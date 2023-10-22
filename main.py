4bb16d93dfcfd6895f8b1f8a74ba8f54fd83dfe8
Reborn Rulz <142952541+rulz-ai@users.noreply.github.com>
2023-10-23 02:06:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,48 +3,24 @@ import json
 import quart
 import quart_cors
 from quart import request
+from rulzai import RulzAI
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todos. Does not persist if Python session is restarted.
-_TODOS = {}
+# Instantiate your Rulz-AI model
+rulz_ai_model = RulzAI()
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps({"todos": _TODOS.get(username, [])}), status=200)
-
-@app.post("/todos/<string:username>")
-async def add_todo(username):
+@app.post("/generate")
+async def generate_text():
     request_data = await request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request_data["todo"])
-    return quart.Response(response='OK', status=200)
+    input_text = request_data["input_text"]
+    
+    # Call your Rulz-AI model to generate the desired text based on the input
+    generated_text = rulz_ai_model.generate_text(input_text)
+    
+    return quart.Response(response=json.dumps({"generated_text": generated_text}), status=200)
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request_data = await request.get_json(force=True)
-    todo_idx = request_data["todo_idx"]
-    if 0 <= todo_idx < len(_TODOS.get(username, [])):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
-
-@app.get("/logo.png")
-async def plugin_logo():
-    filename = 'logo.png'
-    return await quart.send_file(filename, mimetype='image/png')
-
-@app.get("/.well-known/ai-plugin.json")
-async def plugin_manifest():
-    with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/json")
-
-@app.get("/openapi.yaml")
-async def openapi_spec():
-    with open("openapi.yaml") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/yaml")
+# Other necessary routes and functions for your Rulz-AI integration
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
